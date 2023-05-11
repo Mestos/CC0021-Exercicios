faefbdf85c6f21d354022a6f1789de2f9c514723
ALEXANDRE MARQUES CABRAL CABRAL <o.w@camaroes.ldap.local>
2023-05-11 09:49:19 -0300

    MPI- Soma de elementos de vetor corrigido

--- mpi_vetsoma.c
+++ mpi_vetsoma.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define tam 10000
+#define tam 100000000
 
 float *gerar_vetor(int n) {
     float *vetor;
@@ -27,27 +27,27 @@ int main(){
     int i;
     if(rank == 0){
         v = gerar_vetor(tam);
-        MPI_Send(v, tam, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
-        MPI_Send(v, tam, MPI_FLOAT, 2, 0, MPI_COMM_WORLD);
+        MPI_Bcast(v, tam, MPI_FLOAT, 0, MPI_COMM_WORLD);
         MPI_Recv(&soma, 1, MPI_FLOAT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Soma 1: %.5f\n", soma);
         MPI_Recv(&soma, 1, MPI_FLOAT, 2, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Soma 2: %.5f\n", soma);
     }
-    
-    if(rank == 1){
-        MPI_Recv(&soma, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for(i=0; i<tam; i++){
-            soma += v[i];
+    else{
+        v = (float *)malloc(sizeof(float) * tam);
+        MPI_Bcast(v, tam, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(rank == 1){
+            for(i=0; i<tam; i++){
+                soma += v[i];
+            }
+            MPI_Send(&soma, 1, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
         }
-        MPI_Send(&soma, 1, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
-    }
-    if(rank == 2){
-        MPI_Recv(&soma, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for(i=0; i<tam; i++){
-            soma += 2*v[i];
+        if(rank == 2){
+            for(i=0; i<tam; i++){
+                soma += 2*v[i];
+            }
+            MPI_Send(&soma, 1, MPI_FLOAT, 0, 2, MPI_COMM_WORLD);
         }
-        MPI_Send(&soma, 1, MPI_FLOAT, 0, 2, MPI_COMM_WORLD);
     }
     free(v);
     v = NULL;
