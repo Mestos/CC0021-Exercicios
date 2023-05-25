13793064e9e3d86170ebca1fd60dedf2f43fc3b0
Mestos <alemarquesnews@gmail.com>
2023-05-24 22:22:54 -0300

    Projeto: Incompleto 3

--- projeto.c
+++ projeto.c
@@ -23,9 +23,11 @@ int main(){
     int count[max];
     int i;
     int j;
+    double inicio,tempo, fim;
     for (i=0; i<max; i++){
         count[i] = 0;
     }
+    inicio = omp_get_wtime();
     for(i=0; i<tam; i++){
         for(j=0; j<max; j++){
             if (estoque[i] == j){
@@ -33,31 +35,36 @@ int main(){
             }
         }
     }
+    fim = omp_get_wtime();
     for (i=0; i<max; i++){
         printf("Gênero ID %d: %d livros\n", i, count[i]);
     }
+    tempo = fim - inicio;
+    printf("Tempo de execução: %.5f\n", tempo);
     for (i=0; i<max; i++){
         count[i] = 0;
     }
-    printf("\n");
+    printf("\nExecução em paralelo:\n");
+    inicio = omp_get_wtime();
     #pragma omp parallel num_threads(4)
     {
-        int c[max];
-        for (i=0; i<max; i++){
-            c[i] = 0;
-        }
         #pragma omp for
             for(i=0; i<tam; i++){
+                #pragma omp critical
                 for(j=0; j<max; j++){
                     if (estoque[i] == j){
-                        c[j]++;
+                        count[j]++;
                     }
                 }
             }
     }
+    fim = omp_get_wtime();
     for (i=0; i<max; i++){
         printf("Gênero ID %d: %d livros\n", i, count[i]);
     }
+    tempo = fim - inicio;
+    printf("Tempo de execução: %.5f\n", tempo);
+
 
 
     free(estoque);
